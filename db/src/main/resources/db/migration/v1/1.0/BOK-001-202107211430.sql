f6ab39ba548a69df44a7fd8bd413410fd6d146e3
Mihail <mynote02012014@gmail.com>
2021-07-21 15:44:38 +0300

    Sql update

---
+++ db/src/main/resources/db/migration/v1/1.0/BOK-001-202107211430.sql
@@ -0,0 +1,34 @@
+DROP SCHEMA booking CASCADE;
+CREATE SCHEMA booking;
+
+CREATE TABLE booking.admins
+(
+    admin_id   SERIAL      NOT NULL PRIMARY KEY,
+    username   VARCHAR(50) NOT NULL UNIQUE,
+    first_name VARCHAR(50) NOT NULL,
+    last_name  VARCHAR(50) NOT NULL,
+    password   VARCHAR(50) NOT NULL,
+    photo_path VARCHAR(255)
+);
+
+CREATE TABLE booking.users
+(
+    user_id      SERIAL      NOT NULL PRIMARY KEY,
+    username     VARCHAR(50) NOT NULL,
+    first_name   VARCHAR(50) NOT NULL,
+    last_name    VARCHAR(50) NOT NULL,
+    password     VARCHAR(50) NOT NULL,
+    photo_path   VARCHAR(255),
+    phone_number VARCHAR(20) NULL,
+    email        VARCHAR(50) NULL
+
+);
+
+CREATE TABLE booking.user_ratings
+(
+    user_rating_id SERIAL  NOT NULL PRIMARY KEY,
+    target_user_id INTEGER NOT NULL REFERENCES booking.users (user_id),
+    author_user_id INTEGER NOT NULL REFERENCES booking.users (user_id),
+    rating         INTEGER NOT NULL,
+    description    VARCHAR(1000)
+);
