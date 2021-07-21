045b27e9f3c7d754a42c2b73191ca4d9e725b27d
Иван Сметанин <smetaninivanu@gmail.com>
2021-07-21 19:01:17 +0300

    Updated DB structure

---
+++ db/src/main/resources/db/migration/v1/1.0/BOK-002-202107211850.sql
@@ -0,0 +1,30 @@
+CREATE TABLE booking.roles
+(
+    role_id   SERIAL NOT NULL PRIMARY KEY,
+    role_name VARCHAR(50)
+);
+
+ALTER TABLE booking.users
+    ADD COLUMN role_id VARCHAR(50) REFERENCES booking.roles (role_id);
+
+CREATE TABLE booking.equipments
+(
+    equipment_id   SERIAL NOT NULL PRIMARY KEY,
+    equipment_name VARCHAR(255)
+);
+
+CREATE TABLE booking.rooms_equipments
+(
+    room_id      INTEGER NOT NULL REFERENCES booking.rooms (room_id),
+    equipment_id INTEGER NOT NULL REFERENCES booking.equipments (equipment_id),
+    number       INTEGER NOT NULL,
+    PRIMARY KEY (room_id, equipment_id)
+);
+
+CREATE TABLE booking.room_reviews
+(
+    room_review_id SERIAL        NOT NULL PRIMARY KEY,
+    room_id        INTEGER       NOT NULL REFERENCES booking.rooms (room_id),
+    rating         INTEGER       NOT NULL,
+    description    VARCHAR(1000) NOT NULL
+);
