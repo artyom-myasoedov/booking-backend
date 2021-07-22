1e1b602bbd76e973b4876e2817a39a07ca98603e
Mihail <mynote02012014@gmail.com>
2021-07-22 11:16:36 +0300

    Users reviews and room reviews unite in one table in db

---
+++ db/src/main/resources/db/migration/v1/1.0/BOK-001-202107221000.sql
@@ -0,0 +1,19 @@
+ALTER TABLE booking.room_reviews
+    RENAME TO reviews;
+
+CREATE TABLE booking.review_targets(
+    review_target_id SERIAL NOT NULL PRIMARY KEY ,
+    review_target_name VARCHAR(50) NOT NULL
+);
+
+ALTER TABLE booking.reviews
+    RENAME COLUMN room_review_id TO review_id;
+
+ALTER TABLE booking.reviews
+    RENAME COLUMN room_id TO rated_entity_id;
+
+ALTER TABLE booking.reviews
+    RENAME COLUMN author_user_id TO author_id;
+
+ALTER TABLE booking.reviews
+    ADD COLUMN review_target_id INTEGER NOT NULL REFERENCES booking.reviews_targets (review_target_id;
