f5bffb915f8d733b0caad2ca31144f0891eb83e9
Мясоедов Артём <volot36rus@gmail.com>
2021-07-29 12:17:37 +0000

    Delete Controller.java

--- rest/src/main/java/booking/backend/rest/api/Controller.java
+++
@@ -1,9 +0,0 @@
-package booking.backend.rest.api;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@org.springframework.stereotype.Controller
-@RequestMapping
-public class Controller {
-
-}
