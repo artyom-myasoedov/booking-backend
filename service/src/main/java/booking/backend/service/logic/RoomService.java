7da474626c63c8fe522beb0747801127ed464baf
Мясоедов Артём <volot36rus@gmail.com>
2021-08-04 08:21:55 +0000

    Update RoomService.java

--- service/src/main/java/booking/backend/service/logic/RoomService.java
+++ service/src/main/java/booking/backend/service/logic/RoomService.java
@@ -1,5 +1,22 @@
 package booking.backend.service.logic;
 
 
+import booking.backend.service.model.RoomCreateDto;
+import booking.backend.service.model.RoomDto;
+
+import java.util.List;
+
 public interface RoomService {
+
+  RoomDto findById(Integer roomId);
+
+  void deleteById(Integer roomId);
+
+  RoomDto addRoom(RoomCreateDto room);
+
+  RoomDto updateRoom(RoomDto room);
+
+  List<RoomDto> findByLandlordId(Integer landlordId);
+
+  List<RoomDto> findAll();
 }
