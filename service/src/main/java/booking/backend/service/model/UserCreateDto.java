c1f0054ebf75c883d373077cfc57fe200c875475
Ivan Smetanin <smetaninivanu@gmail.com>
2021-08-07 16:24:23 +0300

    Add validation to user

--- service/src/main/java/booking/backend/service/model/UserCreateDto.java
+++ service/src/main/java/booking/backend/service/model/UserCreateDto.java
@@ -3,6 +3,7 @@ package booking.backend.service.model;
 import booking.backend.commons.Role;
 import booking.backend.service.validation.UsernameUnique;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.*;
 
@@ -14,8 +15,8 @@ public class UserCreateDto {
   private String username;
 
   @NotBlank(message = "password.is-blank")
-  @Min(value = 12, message = "password.too-short")
-  @Max(value = 128, message = "password.too-long")
+  @Length(min= 12, message = "password.too-short")
+  @Length(max= 60, message = "password.too-long")
   @JsonProperty("password")
   private String password;
 
@@ -41,4 +42,68 @@ public class UserCreateDto {
 
   @JsonProperty("email")
   private String email;
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public Role getRole() {
+    return role;
+  }
+
+  public void setRole(Role role) {
+    this.role = role;
+  }
+
+  public byte[] getPhoto() {
+    return photo;
+  }
+
+  public void setPhoto(byte[] photo) {
+    this.photo = photo;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
 }
