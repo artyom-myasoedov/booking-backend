73d282eb974364daa2c06e71573fac91967bda90
Ivan Smetanin <smetaninivanu@gmail.com>
2021-08-09 11:24:36 +0300

    Change to roomId

--- db/src/main/java/booking/backend/db/entity/RoomEntity.java
+++ db/src/main/java/booking/backend/db/entity/RoomEntity.java
@@ -33,7 +33,7 @@ import java.util.List;
       name = "review",
       attributeNodes = {
         @NamedAttributeNode("id"),
-        @NamedAttributeNode("ratedEntityId"),
+        @NamedAttributeNode("roomId"),
         @NamedAttributeNode("rating"),
         @NamedAttributeNode("description"),
         @NamedAttributeNode("author"),
@@ -123,7 +123,7 @@ public class RoomEntity {
 
   @OneToMany
   @JoinColumn(
-    name = "rated_entity_id",
+    name = "room_id",
     referencedColumnName = "room_id",
     insertable = false,
     updatable = false
